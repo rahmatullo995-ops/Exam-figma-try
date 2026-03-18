14413e1d9def5aaacaf4df507e8493d4f2a67d3a
rahmatullo995 <rahmatulo@gmail.com>
2026-03-18 17:59:56 +0500

    gt

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
       </Routes>
       <Footer />
     </div>
